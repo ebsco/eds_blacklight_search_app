87c7a97ddf7b92ca4428b478d122fc2656a811a6
Bill McKinney <mckinney.sw@gmail.com>
2017-04-03 11:20:25 -0400

    adds mla, apa and chicago citation formats

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -31,4 +31,23 @@ class SolrDocument
   # and Blacklight::Document::SemanticFields#to_semantic_values
   # Recommendation: Use field names from Dublin Core
   use_extension(Blacklight::Document::DublinCore)
+
+  def export_as_apa_citation_txt
+    doc = self
+    cite = doc['citation_apa']
+    cite.html_safe
+  end
+
+  def export_as_mla_citation_txt
+    doc = self
+    cite = doc['citation_mla']
+    cite.html_safe
+  end
+
+  def export_as_chicago_citation_txt
+    doc = self
+    cite = doc['citation_chicago']
+    cite.html_safe
+  end
+
 end
