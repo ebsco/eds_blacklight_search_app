520d6ff8ae4362e1cb7f9b99dc4c1ccb5805c2b8
Bill McKinney <mckinney.sw@gmail.com>
2017-04-19 15:14:44 -0400

    adds redirect to current_url after login (thanks Eric!)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+
+  def after_sign_in_path_for(resource)
+    if session[:current_url].present?
+      session[:current_url]
+    else
+      root_path
+    end
+  end
+
   layout 'blacklight'
 
   protect_from_forgery with: :exception
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 module ApplicationHelper
 
   def eds_links(options={})
@@ -6,20 +8,42 @@ module ApplicationHelper
       url = link['url']
       target = '_blank'
       label = link['label']
-      if url == 'detail'
-        url = '#'
-        target = nil
-        label = 'Please log in to view ' + label
+      type = link['type']
+      case type
+        when 'pdf', 'ebook-pdf', 'ebook-epub', 'html', 'cataloglink'
+          if url == 'detail' || current_or_guest_user.guest
+            url = '/users/sign_in'
+            target = '_self'
+            label = 'Access is available, login to view'
+          end
+          links = link_to(label, url, target: target)
+        else
+          # do nothing
       end
-      links = link_to(label, url, target: target)
     end
-    links.to_s
+    if links.empty?
+      'None available'
+      else
+        links.to_s
+    end
   end
 
   def doi_link(options={})
     doi = options[:value].first
     url = 'https://doi.org/' + doi.to_s
     link_to( url, url, target: '_blank')
+  end
+
+  def html_fulltext(options={})
+    data = CGI.unescapeHTML options[:value].first
+    data.gsub!('<anid>','<anid style="display: none">')
+    data.gsub!('<title ','<div ')
+    data.gsub!('</title>','</div>')
+    data.gsub!('<bold>','<b>')
+    data.gsub!('</bold>','</b>')
+    data.gsub!('<emph>','<i>')
+    data.gsub!('</emph>','</i>')
+    data.html_safe
   end
 
 end
